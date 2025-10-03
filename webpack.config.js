b625639b4deb5be34ce480ff061b0b22747fc16e
David Tang <david_tang@web.de>
2025-10-03 09:58:56 +0200

    Boilerplate Configuration Set

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,63 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true, // Clean /dist on each build
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: '!!html-loader!templates/index.html'
-    })
-  ],
-  devtool: 'sourcemap',
-  mode: "development",
+  devtool: 'source-map',
   module: {
     rules: [
+      // JS / JSX
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
+      // SCSS / SASS
       {
-        test: /\.s?css$/,
+        test: /\.s[ac]ss$/i,
         exclude: /node_modules/,
-        loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass') // forces Dart Sass
+            }
+          }
+        ]
       },
+      // HTML
       {
         test: /\.html$/,
         loader: 'html-loader'
-      },
+      }
     ]
   },
   resolve: {
-    extensions: [ '.js', '.jsx' ]
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'templates/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    port: 3000,
+    hot: true,
+    open: true,
+    compress: true,
+    historyApiFallback: true
   }
 };

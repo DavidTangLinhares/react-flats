eee61d6978e5b80aebe9a34b7fe5df0bfbc87077
David Tang <david_tang@web.de>
2025-10-08 11:44:38 +0200

    Setup and App

---
+++ src/components/app.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'App'
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div className="flat-list">
+          <div className='card' style={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg')"}}>
+            <div className="card-category">150 EUR</div>
+            <div className="card-description">
+              <h2>Super 60m2 in trendy neighborhood!</h2>
+            </div>
+            <a class="card-link" href="#"></a>
+          </div>
+          <div className='card' style={{backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg')"}}>
+            <div className="card-category">150 EUR</div>
+            <div className="card-description">
+              <h2>Super 60m2 in trendy neighborhood!</h2>
+            </div>
+            <a class="card-link" href="#"></a>
+          </div>
+        </div>
+        <div className="map-container">
+          hello Map
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

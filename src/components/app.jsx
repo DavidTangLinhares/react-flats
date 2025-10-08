f408841221ff8f51ed6762bcfdf396b03acb5323
David Tang <david_tang@web.de>
2025-10-08 12:15:41 +0200

    Google Map Added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SimpleMap from './google-map-react';
 
 class App extends Component {
   constructor(props) {
@@ -28,7 +29,7 @@ class App extends Component {
           </div>
         </div>
         <div className="map-container">
-          hello Map
+          <SimpleMap />
         </div>
       </div>
     );
